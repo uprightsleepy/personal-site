2da0839a2f2a88ab82e4e2356d50ed9bec5ce851
uprightsleepy <henry@designedbyhenryp.com>
2025-09-03 17:13:31 -0500

    remove contact page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -22,11 +22,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.6,
-    },
   ]
 }
